0ec43381fdb57e680a5d683b1f732250839b6ec3
Tuğbay Atilla <tugbayatilla@hotmail.com>
2018-11-21 14:01:06 +0300

    toList extension method added

---
+++ ArchPM.ApiQuery.AuthConfigSection.Tests/ExtensionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ArchPM.ApiQuery.AuthConfigSection.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArchPM.ApiQuery.AuthConfigSection.Tests
+{
+    [TestClass]
+    public class ExtensionTests
+    {
+        [TestMethod]
+        public void Extension_ToList()
+        {
+            var manager = ApiQuerySectionManager.Initialize("ValidSectionSample");
+
+            var infos = manager.Section.EnvironmentInfos.ToList<EnvironmentInfoElement>();
+
+            Assert.AreEqual(3, infos.Count);
+            Assert.AreEqual(AuthConfigSectionEnvironments.Dev, infos[0].Env);
+            Assert.AreEqual(AuthConfigSectionEnvironments.UAT, infos[1].Env);
+            Assert.AreEqual(AuthConfigSectionEnvironments.Prod, infos[2].Env);
+
+
+        }
+
+
+    }
+}
---
+++ ArchPM.ApiQuery.AuthConfigSection/Model/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchPM.ApiQuery.AuthConfigSection.Model
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class AuthConfigSectionExtensions
+    {
+        /// <summary>
+        /// To the list.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection">The collection.</param>
+        /// <returns></returns>
+        public static List<T> ToList<T>(this ConfigurationElementCollection collection)
+        {
+            List<T> result = new List<T>();
+            var q = collection.GetEnumerator();
+
+            while (q.MoveNext())
+            {
+                result.Add((T)q.Current);
+            }
+            return result;
+        }
+    }
+}
